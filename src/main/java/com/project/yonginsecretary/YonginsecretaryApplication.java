1abe79260bf4437da8471895c94793f46e38c7ca
geonhoon <geonhoon0403@naver.com>
2022-10-26 05:07:21 +0900

    first commit

---
+++ src/main/java/com/project/yonginsecretary/YonginsecretaryApplication.java
@@ -0,0 +1,17 @@
+package com.project.yonginsecretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.persistence.Entity;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class YonginsecretaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonginsecretaryApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/yonginsecretary/repository/Actor.java
@@ -0,0 +1,4 @@
+package com.project.yonginsecretary.repository;
+
+public class Actor {
+}
